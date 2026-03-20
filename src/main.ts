6eb307a8c36fec4796988d0f8a13fdbb0b59c90e
Tanvir Ishrak <tanvirishrak04@gmail.com>
2026-03-20 17:55:50 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,11 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [
+    'http://localhost:3000',
+    'https://golpo.kafaahbd.com',
+    'http://golpo.kafaahbd.com'
+  ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
